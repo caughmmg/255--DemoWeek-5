e3293478bdd11d970bd42b319d91d9142583238d
Michael Caughman <Caughmmg1993@gmail.com>
2019-09-24 16:46:38 -0400

    Demo Week 5 Notes

---
+++ 255- DemoWeek5/Week 5 Demo/Assets/Scripts/BoxController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class BoxController : MonoBehaviour{
+
+    [Range (-10, 10)]public float speed = 2;
+    public bool shouldFlash = true;
+    MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MeshRenderer meshRenderer = (MeshRenderer)GetComponent(MeshRenderer");
+        //MeshRenderer meshRenderer = (MeshRenderer)GetComponent(typeof(MeshRenderer));
+        meshRenderer = GetComponent<MeshRenderer>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (meshRenderer != null && shouldFlash)
+        {
+            float r = Random.Range(0, 1f);
+            float g = Random.Range(0, 1f);
+            float b = Random.Range(0, 1f);
+
+            meshRenderer.material.color = new Color(r, g, b);
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        /*
+        transform.position += Vector3.right * Time.deltaTime * h * speed;
+        transform.position += Vector3.up * Time.deltaTime * v * speed;
+        */
+
+        transform.position += new Vector3(h, v, 0) * Time.deltaTime * speed;
+        
+    }
+
+    void OnDestroy()
+    {
+
+
+    }
+}
---
+++ 255- DemoWeek5/Week 5 Demo/Assets/Scripts/ParticalController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticalController : MonoBehaviour
+{
+
+    Vector3 velocity = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Random.onUnitSphere * 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity += new Vector3(0, -10, 0)* Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        if(transform.position.y < -10)
+        {
+
+            Destroy(gameObject);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+        print("Clicked on a particle");
+    }
+}
